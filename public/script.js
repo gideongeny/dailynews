74ca262f6ffe48c5c4f84b833687eb641ce03284
gideongeny <gideongeny.github.io@example.com>
2026-01-14 04:44:13 +0300

    UI Polish: Professional SVG search icon and Trending Bar swallow effect.

--- public/script.js
+++ public/script.js
@@ -1907,7 +1907,11 @@ function initSearch() {
     searchBar.innerHTML = `
         <form class="search-form">
             <input type="search" id="header-search-input" placeholder="Search news..." name="q" autocomplete="off">
-            <button type="submit">🔍</button>
+            <button type="submit">
+                <svg class="search-icon-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
         </form>
     `;
     brandingInner.appendChild(searchBar);
